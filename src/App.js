0d1a803794ca2ef0b51f90c920aa6fc14c25e4e4
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2022-12-06 00:40:43 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator, useTheme, View, Image } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import { awsExport } from './aws-exports';
+Amplify.configure(awsExport);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign='center' padding={tokens.space.large}>
+        <Image alt='logo' src='' />
+      </View>
+    );
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator loginMechanisms={['email']} components={components}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
---
+++ src/aws-exports.js
@@ -0,0 +1,6 @@
+export const awsExport = {
+  Auth: {
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+  },
+};
